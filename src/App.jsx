99ea35f7bc2646d7bc6a4a77428600f662821b15
Mamidisetty <127787361+Mamidisetty@users.noreply.github.com>
2025-02-11 00:06:22 +0530

    Frontend devolpment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 class="text-6xl font-italic underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
