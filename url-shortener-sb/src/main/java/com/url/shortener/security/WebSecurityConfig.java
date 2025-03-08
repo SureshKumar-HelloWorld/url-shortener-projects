64d2b2dff170a7b9eb0e52e3d71b8fe7f54069b7
Mamidisetty <127787361+Mamidisetty@users.noreply.github.com>
2025-03-08 23:45:16 +0530

    Modified WebSecurityConfig file

--- url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
+++ url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->auth
-//
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/urls/**").permitAll()
                         .requestMatchers("/{shortUrl}").permitAll()
